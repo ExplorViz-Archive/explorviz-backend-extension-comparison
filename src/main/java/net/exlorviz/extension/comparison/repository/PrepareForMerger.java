87d440dea2712ae27b1dabccc6f6c5062dfd6429
Josefine Wegert <jweg@informatik.uni-kiel.de>
2018-01-29 18:21:42 +0100

    status added

---
+++ src/main/java/net/exlorviz/extension/comparison/repository/PrepareForMerger.java
@@ -0,0 +1,68 @@
+package net.exlorviz.extension.comparison.repository;
+
+import java.util.List;
+
+import net.explorviz.extension.comparison.model.Status;
+import net.explorviz.model.Application;
+import net.explorviz.model.Clazz;
+import net.explorviz.model.CommunicationClazz;
+import net.explorviz.model.Component;
+import net.explorviz.model.Landscape;
+
+/**
+ * Currently extends an {@link Application} by a {@link Status}. The
+ * {@link Status} is necessary to merge two {@link Application}s. In future
+ * {@link Landscape}s should be merged, too.
+ *
+ * @author josw
+ *
+ */
+public class PrepareForMerger {
+
+	/**
+	 * Adds a {@link Status} attribute to every element in the {@link Application}.
+	 * This serves as preparation for merging two {@link Application}s.
+	 *
+	 * @param mergingApp
+	 *            {@link Application} that should be merged without status attribute
+	 * @return {@link Application} that should be merged with status attribute
+	 */
+	public Application addStatusToApp(final Application mergingApp) {
+
+		for (final CommunicationClazz communication : mergingApp.getCommunications()) {
+			communication.getExtensionAttributes().put("status", Status.ORIGINAL);
+		}
+
+		for (final Component component : mergingApp.getComponents()) {
+			component.getExtensionAttributes().put("status", Status.ORIGINAL);
+
+			for (final Clazz clazz : component.getClazzes()) {
+				clazz.getExtensionAttributes().put("status", Status.ORIGINAL);
+			}
+
+			this.componentRecursive(component.getChildren());
+		}
+
+		return mergingApp;
+	}
+
+	/**
+	 * Helps the {@link #addStatusToApp(Application)} method to add the status
+	 * recursively to all children of {@link Component}s.
+	 *
+	 * @param components
+	 *            list of child components
+	 */
+	private void componentRecursive(final List<Component> components) {
+		for (final Component component : components) {
+			component.getExtensionAttributes().put("status", Status.ORIGINAL);
+
+			for (final Clazz clazz : component.getClazzes()) {
+				clazz.getExtensionAttributes().put("status", Status.ORIGINAL);
+			}
+
+			this.componentRecursive(component.getChildren());
+
+		}
+	}
+}
--- src/main/java/net/explorviz/extension/comparison/model/Status.java
+++ src/main/java/net/explorviz/extension/comparison/model/Status.java
@@ -2,12 +2,12 @@ package net.explorviz.extension.comparison.model;
 
 import java.io.Serializable;
 
-import net.exlorviz.extension.comparison.repository.AppMerger;
+import net.exlorviz.extension.comparison.repository.Merger;
 
 /**
  * This flag is attached to each entity of the meta-model. It shows whether an
  * entity is modified. This information is needed for building the
- * comparing-application object {@link AppMerger}.(static view) Plus it holds
+ * comparing-application object {@link Merger}.(static view) Plus it holds
  * the difference of class instances and communication requests between the two
  * models.(dynamic view)
  *
