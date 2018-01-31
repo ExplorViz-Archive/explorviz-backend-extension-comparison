2a1dc680222384c17548cafc53ffccece640e7da
Josefine Wegert <jweg@informatik.uni-kiel.de>
2018-01-31 22:21:09 +0100

    bug fixed: register meta-model from backend for providing resource

--- src/main/java/net/explorviz/extension/comparison/main/Application.java
+++ src/main/java/net/explorviz/extension/comparison/main/Application.java
@@ -9,11 +9,39 @@ import org.glassfish.jersey.server.ResourceConfig;
 import com.github.jasminb.jsonapi.ResourceConverter;
 
 import net.explorviz.injection.ResourceConverterFactory;
+import net.explorviz.model.Clazz;
+import net.explorviz.model.Communication;
+import net.explorviz.model.CommunicationClazz;
+import net.explorviz.model.Component;
+import net.explorviz.model.DatabaseQuery;
+import net.explorviz.model.Landscape;
+import net.explorviz.model.Node;
+import net.explorviz.model.NodeGroup;
+import net.explorviz.model.Timestamp;
+import net.explorviz.model.helper.CommunicationAccumulator;
+import net.explorviz.model.helper.CommunicationTileAccumulator;
+import net.explorviz.server.providers.GenericTypeFinder;
+import net.explorviz.server.security.User;
 
 @ApplicationPath("/extension/comparison")
 public class Application extends ResourceConfig {
 
 	public Application() {
+
+		GenericTypeFinder.typeMap.putIfAbsent("Timestamp", Timestamp.class);
+		GenericTypeFinder.typeMap.putIfAbsent("Landscape", Landscape.class);
+		GenericTypeFinder.typeMap.putIfAbsent("System", net.explorviz.model.System.class);
+		GenericTypeFinder.typeMap.putIfAbsent("NodeGroup", NodeGroup.class);
+		GenericTypeFinder.typeMap.putIfAbsent("Node", Node.class);
+		GenericTypeFinder.typeMap.putIfAbsent("Application", Application.class);
+		GenericTypeFinder.typeMap.putIfAbsent("Component", Component.class);
+		GenericTypeFinder.typeMap.putIfAbsent("Clazz", Clazz.class);
+		GenericTypeFinder.typeMap.putIfAbsent("CommunicationClazz", CommunicationClazz.class);
+		GenericTypeFinder.typeMap.putIfAbsent("Communication", Communication.class);
+		GenericTypeFinder.typeMap.putIfAbsent("CommunicationAccumulator", CommunicationAccumulator.class);
+		GenericTypeFinder.typeMap.putIfAbsent("CommunicationTileAccumulator", CommunicationTileAccumulator.class);
+		GenericTypeFinder.typeMap.putIfAbsent("DatabaseQuery", DatabaseQuery.class);
+		GenericTypeFinder.typeMap.putIfAbsent("User", User.class);
 
 		// register the models that you wan't to parse to JSONAPI-conform JSON,
 		// i.e. exchange with frontend
