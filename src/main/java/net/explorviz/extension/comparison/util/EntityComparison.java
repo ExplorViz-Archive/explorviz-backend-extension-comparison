12227c045733e4b1b3611434f3bc6dc3d253c175
Josefine Wegert <jweg@informatik.uni-kiel.de>
2018-01-31 22:23:28 +0100

    component comparing method + help functions implemented and tested

--- src/main/java/net/explorviz/extension/comparison/util/EntityComparison.java
+++ src/main/java/net/explorviz/extension/comparison/util/EntityComparison.java
@@ -2,34 +2,147 @@ package net.explorviz.extension.comparison.util;
 
 import java.util.List;
 
+import net.explorviz.model.Clazz;
 import net.explorviz.model.Communication;
 import net.explorviz.model.Component;
+import net.explorviz.model.helper.Draw3DNodeEntity;
 
 /**
  * Class for methods that compare elements from the meta-model, e.g.
  * {@link Component}, {@link Communication}.
- * 
+ *
  * @author josw
  *
  */
 public class EntityComparison {
 
-	public boolean componentsEqual(final Component component1, final Component component2) {
+	boolean fullNameEqual = false;
+	boolean clazzesEqual = false;
+	boolean childrenEqual = false;
 
-		boolean componentsEqual = false;
+	/**
+	 * Two {@link Component}s are identical, if they have the same
+	 * fullQualifiedName, the same children and {@link Clazz}es. The children and
+	 * {@link Clazz}es may be empty.
+	 *
+	 * @param component1
+	 * @param component2
+	 * @return true: if {@link Component}s are identical, false: else
+	 */
+	public boolean componentsIdentical(final Component component1, final Component component2) {
+		boolean componentsIdentical = false;
 
 		final String fullName1 = component1.getFullQualifiedName();
 		final String fullName2 = component2.getFullQualifiedName();
-		final long timestamp1 = component1.getTimestamp();
-		final long timestamp2 = component2.getTimestamp();
 
-		if ((fullName1.equals(fullName2)) && (timestamp1 != timestamp2)) {
-			componentsEqual = true;
+		final List<Clazz> clazzes1 = component1.getClazzes();
+		final List<Clazz> clazzes2 = component2.getClazzes();
+		final List<Component> children1 = component1.getChildren();
+		final List<Component> children2 = component2.getChildren();
+
+		// TODO && (timestamp1 != timestamp2) not for testing, because timestamp=0, in
+		// example landscape
+		if ((fullName1.equals(fullName2))) {
+			// same name
+			if ((clazzes1.size() == 0) && (clazzes2.size() == 0)) {
+
+				if ((children1.size() == 0) && (children2.size() == 0)) {
+					// empty clazzes and empty children
+					componentsIdentical = true;
+				} else if ((children1.size() == children2.size())) {
+					// empty clazzes, but children
+					componentsIdentical = this.childrenEqual(children1, children2);
+				}
+			} else if (clazzes1.size() == clazzes2.size()) {
+				// not empty clazzes
+				clazzesEqual = this.clazzesEqual(clazzes1, clazzes2);
+				if (clazzesEqual && (children1.size() > 0) && (children1.size() == children2.size())) {
+					// same clazzes and not empty children
+					componentsIdentical = childrenEqual(children1, children2);
+				}
+				if (clazzesEqual && children1.size() == 0 && children2.size() == 0) {
+					// same clazzes, but empty children
+					componentsIdentical = true;
+				}
+			}
 		}
-		return componentsEqual;
+		return componentsIdentical;
 	}
 
-	// TODO parameter more general, problem with casting in AppMerger.java:
+	/**
+	 * Help function that checks whether the children of two {@link Component}s are
+	 * identical. If only one pair of children is not identical, then the children
+	 * of the {@link Component}s are marked as not identical.
+	 *
+	 * @param children1
+	 * @param children2
+	 * @return true: if all children of two {@link Component}s are identical, false:
+	 *         else
+	 */
+	public boolean childrenEqual(final List<Component> children1, final List<Component> children2) {
+		boolean childrenEqual = true;
+
+		for (int i = 0; i < children1.size(); i++) {
+			childrenEqual = this.componentsIdentical(children1.get(i), children2.get(i));
+
+			if (!childrenEqual) {
+				childrenEqual = false;
+				break;
+			}
+		}
+
+		return childrenEqual;
+	}
+
+	/**
+	 * Two List<{@link Clazz}> are equal, if they contain the same elements in the
+	 * same order.
+	 *
+	 * @param clazzes1
+	 *            List<{@link Clazz}>
+	 * @param clazzes2
+	 *            List<{@link Clazz}>
+	 * @return true: if List<{@link Clazz}>1 and List<{@link Clazz}>2 are equal,
+	 *         false:else
+	 */
+	public boolean clazzesEqual(final List<Clazz> clazzes1, final List<Clazz> clazzes2) {
+		boolean clazzesEqual = true;
+		boolean clazzEqual = true;
+
+		for (int i = 0; clazzes1.size() > i; i++) {
+			clazzEqual = elemEqual(clazzes1.get(i), clazzes2.get(i));
+			if (!clazzEqual) {
+				clazzesEqual = false;
+				break;
+			}
+		}
+
+		return clazzesEqual;
+	}
+
+	/**
+	 * Two {@link Draw3DNodeEntity} are equal, if they have the same type and the
+	 * same fullQualifiedName.
+	 *
+	 * @param elem1
+	 * @param elem2
+	 * @return true: if elem1 and elem2 are equal, false:else
+	 */
+	public boolean elemEqual(final Draw3DNodeEntity elem1, final Draw3DNodeEntity elem2) {
+		boolean elemsEqual = false;
+
+		final String fullName1 = elem1.getFullQualifiedName();
+		final String fullname2 = elem2.getFullQualifiedName();
+
+		if (elem1.getClass().equals(elem2.getClass()) && fullName1.equals(fullname2)) {
+			elemsEqual = true;
+		}
+
+		return elemsEqual;
+	}
+
+	// help methods
+	// TODO parameter more general, problem with casting in Merger.java:
 	// List<Draw3DNodeEntity>
 	public boolean containsFullQualifiedName(final List<Component> list, final String fullName) {
 		return list.stream().filter(e -> e.getFullQualifiedName().equals(fullName)).findFirst().isPresent();
---
+++ src/test/java/net/explorviz/extension/comparison/util/EntityComparisonTest.java
@@ -0,0 +1,130 @@
+package net.explorviz.extension.comparison.util;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import net.explorviz.model.Clazz;
+import net.explorviz.model.Component;
+
+public class EntityComparisonTest {
+
+	private final EntityComparison entityComparison = new EntityComparison();
+
+	private final Clazz clazz1a = new Clazz();
+	private final Clazz clazz1b = new Clazz();
+	private final Clazz clazz2 = new Clazz();
+	private final Clazz clazz3 = new Clazz();
+	private final Clazz clazz4 = new Clazz();
+	private final Component clazzComp2 = new Component();
+
+	private final Component comp1a = new Component();
+	private final Component comp1b = new Component();
+	private final Component comp2a = new Component();
+	private final Component comp2b = new Component();
+	private final Component comp2c = new Component();
+	private final Component comp3a = new Component();
+	private final Component comp3b = new Component();
+	private final Component comp3c = new Component();
+
+	private final List<Clazz> clazzes1a23 = new ArrayList<>();
+	private final List<Clazz> clazzes1b23 = new ArrayList<>();
+	private final List<Clazz> clazzes1a2 = new ArrayList<>();
+	private final List<Clazz> clazzes1a34 = new ArrayList<>();
+
+	private final List<Component> children1a = new ArrayList<>();
+	private final List<Component> children1b = new ArrayList<>();
+	private final List<Component> children1c = new ArrayList<>();
+
+	@Before
+	public void setUpElements() {
+		clazz1a.setFullQualifiedName("clazz1");
+		clazz1b.setFullQualifiedName("clazz1");
+		clazz2.setFullQualifiedName("clazz2");
+		clazz3.setFullQualifiedName("clazz3");
+		clazz3.setFullQualifiedName("clazz4");
+
+		clazzComp2.setFullQualifiedName("clazz2");
+
+		comp1a.setFullQualifiedName("comp1");
+		comp1b.setFullQualifiedName("comp1");
+		// with clazzes
+		comp2a.setFullQualifiedName("comp2");
+		comp2a.setClazzes(clazzes1a23);
+		comp2b.setFullQualifiedName("comp2");
+		comp2b.setClazzes(clazzes1b23);
+		comp2c.setFullQualifiedName("comp2");
+		comp2c.setClazzes(clazzes1a34);
+		// with children
+		comp3a.setFullQualifiedName("comp3");
+		comp3a.setChildren(children1a);
+		comp3b.setFullQualifiedName("comp3");
+		comp3b.setChildren(children1b);
+		comp3c.setFullQualifiedName("comp3");
+		comp3c.setChildren(children1c);
+
+		clazzes1a23.add(clazz1a);
+		clazzes1a23.add(clazz2);
+		clazzes1a23.add(clazz3);
+
+		clazzes1b23.add(clazz1b);
+		clazzes1b23.add(clazz2);
+		clazzes1b23.add(clazz3);
+
+		clazzes1a2.add(clazz1a);
+		clazzes1a2.add(clazz2);
+
+		clazzes1a34.add(clazz1a);
+		clazzes1a34.add(clazz3);
+		clazzes1a34.add(clazz4);
+
+		children1a.add(comp1a);
+		children1a.add(comp2a);
+		children1b.add(comp1b);
+		children1b.add(comp2b);
+		children1c.add(comp1a);
+		children1c.add(comp2c);
+	}
+
+	@Test
+	public void testComponentsEqual() {
+		final boolean simpleCompEqual = entityComparison.componentsIdentical(comp1a, comp1b);
+		final boolean simpleCompUnequal = entityComparison.componentsIdentical(comp1a, clazzComp2);
+		final boolean compWithClazzesEqual = entityComparison.componentsIdentical(comp2a, comp2b);
+		final boolean compWithClazzesUnequal = entityComparison.componentsIdentical(comp2a, comp2c);
+		final boolean compWithSubCompsEqual = entityComparison.componentsIdentical(comp3a, comp3b);
+		final boolean compWithSubCompsUnequal = entityComparison.componentsIdentical(comp3a, comp3c);
+
+		assertEquals(true, simpleCompEqual);
+		assertEquals(false, simpleCompUnequal);
+		assertEquals(true, compWithClazzesEqual);
+		assertEquals(false, compWithClazzesUnequal);
+		assertEquals(true, compWithSubCompsEqual);
+		assertEquals(false, compWithSubCompsUnequal);
+	}
+
+	@Test
+	public void testClazzesEqual() {
+		final boolean clazzesEqual = entityComparison.clazzesEqual(clazzes1a23, clazzes1b23);
+		final boolean clazzesUnequal = entityComparison.clazzesEqual(clazzes1a23, clazzes1a34);
+
+		assertEquals(true, clazzesEqual);
+		assertEquals(false, clazzesUnequal);
+	}
+
+	@Test
+	public void testElemEqual() {
+		final boolean clazzEqual = entityComparison.elemEqual(clazz1a, clazz1b);
+		final boolean clazzUnequal = entityComparison.elemEqual(clazz2, clazz3);
+		final boolean clazzCompUnequal = entityComparison.elemEqual(clazz2, clazzComp2);
+
+		assertEquals(true, clazzEqual);
+		assertEquals(false, clazzUnequal);
+		assertEquals(false, clazzCompUnequal);
+	}
+
+}
