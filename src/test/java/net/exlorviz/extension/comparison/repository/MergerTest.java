26fe8a24f9e9f0094f90d496af2c5f67cdb8c584
Josefine Wegert <jweg@informatik.uni-kiel.de>
2018-02-01 16:59:43 +0100

    test added

---
+++ src/test/java/net/exlorviz/extension/comparison/repository/MergerTest.java
@@ -0,0 +1,51 @@
+package net.exlorviz.extension.comparison.repository;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import net.explorviz.extension.comparison.model.Status;
+import net.explorviz.model.Application;
+import net.explorviz.model.Clazz;
+import net.explorviz.model.Node;
+
+public class MergerTest {
+
+	private final Merger merger = new Merger();
+
+	private Application application1 = new Application();
+	private Application application2 = new Application();
+	private Application mergedApplication = new Application();
+	private final Node dummyNode = new Node();
+
+	/**
+	 * For more information on how the two {@link Application}s differ, take a look
+	 * at {@link LandscapeExampleCreator#createSimpleApplicationVersion1(Node)} and
+	 * {@link LandscapeExampleCreator#createSimpleApplicationVersion2(Node)}.
+	 */
+	@Before
+	public void setUpApps() {
+		dummyNode.setName("dummyNode");
+		application1 = LandscapeExampleCreator.createSimpleApplicationVersion1(dummyNode);
+		application2 = LandscapeExampleCreator.createSimpleApplicationVersion2(dummyNode);
+
+	}
+
+	@Test
+	public void testAppMerge() {
+		mergedApplication = merger.appMerge(application1, application2);
+
+		// clazz: orgV1.demoV1
+		final Clazz originalClazz = mergedApplication.getComponents().get(0).getClazzes().get(0);
+
+		// expected status = ORIGINAL
+		assertEquals(originalClazz.getExtensionAttributes().get("status"), Status.ORIGINAL);
+
+		// expected status = ADDED
+
+		// expected status = EDITED
+
+	}
+
+}
