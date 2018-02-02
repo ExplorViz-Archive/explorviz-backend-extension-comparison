ed088b33b11fc9ae598d86c52a45276fe564519b
Josefine Wegert <jweg@informatik.uni-kiel.de>
2018-02-02 14:29:57 +0100

    test added

--- src/test/java/net/exlorviz/extension/comparison/repository/MergerTest.java
+++ src/test/java/net/exlorviz/extension/comparison/repository/MergerTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import net.explorviz.extension.comparison.model.Status;
 import net.explorviz.model.Application;
 import net.explorviz.model.Clazz;
+import net.explorviz.model.Component;
 import net.explorviz.model.Node;
 
 public class MergerTest {
@@ -34,17 +35,29 @@ public class MergerTest {
 
 	@Test
 	public void testAppMerge() {
+		mergedApplication = application2;
 		mergedApplication = merger.appMerge(application1, application2);
 
 		// clazz: orgV1.demoV1
 		final Clazz originalClazz = mergedApplication.getComponents().get(0).getClazzes().get(0);
+		// TODO component: ? ORIGINAL
+		// clazz: netV2.subOrgV1.subDemoClass2
+		final Clazz addedClazz = mergedApplication.getComponents().get(1).getChildren().get(0).getClazzes().get(0);
+		// component: netV2
+		final Component addedComponent = mergedApplication.getComponents().get(1);
+		// TODO clazz: ? EDITED
+		// component: orgV1
+		final Component editedComponent = mergedApplication.getComponents().get(0);
 
 		// expected status = ORIGINAL
-		assertEquals(originalClazz.getExtensionAttributes().get("status"), Status.ORIGINAL);
+		assertEquals(Status.ORIGINAL, originalClazz.getExtensionAttributes().get("status"));
 
 		// expected status = ADDED
+		assertEquals(Status.ADDED, addedClazz.getExtensionAttributes().get("status"));
+		assertEquals(Status.ADDED, addedComponent.getExtensionAttributes().get("status"));
 
 		// expected status = EDITED
+		assertEquals(editedComponent.getExtensionAttributes().get("status"), Status.EDITED);
 
 	}
 
