245956b742aa8900aecf5bceb3163d24443cb4f3
Josefine Wegert <jweg@informatik.uni-kiel.de>
2018-04-10 10:29:34 +0200

    retrieve timestamps for merging landscape

--- src/main/java/net/explorviz/extension/comparison/resources/LandscapeResourceComparing.java
+++ src/main/java/net/explorviz/extension/comparison/resources/LandscapeResourceComparing.java
@@ -1,15 +1,13 @@
 package net.explorviz.extension.comparison.resources;
 
-import java.util.List;
-
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import net.exlorviz.extension.comparison.repository.LandscapeFetchService;
 import net.explorviz.model.landscape.Landscape;
-import net.explorviz.model.store.Timestamp;
 import net.explorviz.server.security.Secured;
 
 @Secured
@@ -25,44 +23,14 @@ public class LandscapeResourceComparing {
 
 	@Produces("application/vnd.api+json")
 	@GET
-	@Path("/firstCompare")
-	public Landscape getBeforeLandscape() {
-		final List<Timestamp> filteredTimestamps = service.filterTwoTimestampsForComparison();
-		final Landscape firstLandscape = service.fetchLandscapeForComparison(filteredTimestamps.get(0));
-		// TODO this is just the example landscape used for developing
-		// final Landscape firstLandscape =
-		// LandscapeExampleCreator.createSimpleLandscapeVersion1();
-		return firstLandscape;
+	@Path("/merged/{timestamps}")
+	public Landscape getMergedLandscape(@PathParam("timestamps") final String timestamps) {
+		final String[] timestampsString = timestamps.split(",");
 
-	}
+		final Landscape firstLandscape = service.fetchLandscapeForComparison(Long.parseLong(timestampsString[0]));
+		final Landscape secondLandscape = service.fetchLandscapeForComparison(Long.parseLong(timestampsString[1]));
 
-	@Produces("application/vnd.api+json")
-	@GET
-	@Path("/secondCompare")
-	public Landscape getAfterLandscape() {
-		final List<Timestamp> filteredTimestamps = service.filterTwoTimestampsForComparison();
-		final Landscape secondLandscape = service.fetchLandscapeForComparison(filteredTimestamps.get(1));
-		// TODO this is just the example landscape used for developing
-		// final Landscape secondLandscape =
-		// LandscapeExampleCreator.createSimpleLandscapeVersion2();
-		return secondLandscape;
-
-	}
-
-	@Produces("application/vnd.api+json")
-	@GET
-	@Path("/merged")
-	public Landscape getMergedLandscape() {
-		final List<Timestamp> filteredTimestamps = service.filterTwoTimestampsForComparison();
-		final Landscape firstLandscape = service.fetchLandscapeForComparison(filteredTimestamps.get(0));
-		final Landscape secondLandscape = service.fetchLandscapeForComparison(filteredTimestamps.get(1));
-		// TODO this is just the example landscape used for developing
-		// final Landscape firstLandscape =
-		// LandscapeExampleCreator.createSimpleLandscapeVersion1();
-		// final Landscape secondLandscape =
-		// LandscapeExampleCreator.createSimpleLandscapeVersion2();
 		return service.fetchMergedLandscape(firstLandscape, secondLandscape);
-
 	}
 
 }
