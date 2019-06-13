fbcf64906fb444dcc5319a6810ce07ad61f1eff2
Daniel Teut <danielteut@t-online.de>
2019-06-13 16:47:40 +0200

    Copied and renamed dummy extension

---
+++ src/main/java/net/explorviz/extension/comparison/main/Application.java
@@ -0,0 +1,45 @@
+package net.explorviz.extension.comparison.main;
+
+import net.explorviz.extension.comparison.model.DummyModel;
+import net.explorviz.extension.comparison.model.SubDummyModel;
+import net.explorviz.extension.comparison.resources.TestResource;
+import net.explorviz.shared.common.provider.GenericTypeFinder;
+import net.explorviz.shared.common.provider.JsonApiListProvider;
+import net.explorviz.shared.common.provider.JsonApiProvider;
+import net.explorviz.shared.landscape.model.helper.TypeProvider;
+import net.explorviz.shared.security.filters.AuthenticationFilter;
+import net.explorviz.shared.security.filters.AuthorizationFilter;
+import net.explorviz.shared.security.filters.CorsResponseFilter;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class Application extends ResourceConfig {
+
+  public Application() {
+
+    GenericTypeFinder.getTypeMap().put("DummyModel", DummyModel.class);
+    GenericTypeFinder.getTypeMap().put("SubDummyModel", SubDummyModel.class);
+
+    // register Landscape Model classes, since we want to use them
+    TypeProvider.getExplorVizCoreTypesAsMap().forEach((classname, classRef) -> {
+      GenericTypeFinder.getTypeMap().put(classname, classRef);
+    });
+
+    // register DI
+    register(new DependencyInjectionBinder());
+
+    // Security
+    this.register(AuthenticationFilter.class);
+    this.register(AuthorizationFilter.class);
+    this.register(CorsResponseFilter.class);
+
+    // register providers
+    this.register(JsonApiProvider.class);
+    this.register(JsonApiListProvider.class);
+
+    // register the TestResource
+    register(TestResource.class);
+
+    // Starting point for your DI-based extension
+    this.register(SetupApplicationListener.class);
+  }
+}
---
+++ src/main/java/net/explorviz/extension/comparison/main/DependencyInjectionBinder.java
@@ -0,0 +1,32 @@
+package net.explorviz.extension.comparison.main;
+
+import javax.inject.Singleton;
+import net.explorviz.extension.comparison.services.DummyService;
+import net.explorviz.extension.comparison.services.KafkaLandscapeExchangeService;
+import net.explorviz.extension.comparison.services.LandscapeSerializationHelper;
+import net.explorviz.shared.common.injection.CommonDependencyInjectionBinder;
+
+/**
+ * The DependencyInjectionBinder is used to register Contexts and Dependency Injection (CDI) aspects
+ * for this application.
+ */
+public class DependencyInjectionBinder extends CommonDependencyInjectionBinder {
+
+  @Override
+  public void configure() {
+
+    // Common DI
+    super.configure();
+
+    // Service-specific DI
+    
+    this.bind(DummyService.class).to(DummyService.class).in(Singleton.class);
+
+    this.bind(KafkaLandscapeExchangeService.class).to(KafkaLandscapeExchangeService.class)
+        .in(Singleton.class);
+
+    this.bind(LandscapeSerializationHelper.class).to(LandscapeSerializationHelper.class)
+        .in(Singleton.class);
+
+  }
+}
